e9f99782efb242cd7766ffc2b5861ec5f724430c
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-06 13:42:53 -0500

    creates abstract class 'Shape'

---
+++ QuiltingPatternApp/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiltingPatternApp
+{
+    public abstract class Shape
+    {
+        public int numberOfSides  { get; set; }
+        public int Length { get; set; }
+        public string Color { get; set; }
+    }
+}
