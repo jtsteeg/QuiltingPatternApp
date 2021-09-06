82d0f4e9e918f86e7d2f2a852b26ac8879ffb075
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-06 14:55:47 -0500

    adds ability for program to create triangle objects

--- QuiltingPatternApp/Program.cs
+++ QuiltingPatternApp/Program.cs
@@ -7,8 +7,11 @@ namespace QuiltingPatternApp
         static void Main(string[] args)
         {
             bool isRunning = true;
+            int shapeLength;
+            string shapecolor;
 
-            Console.WriteLine("Welcome to the quilting pattern app! Enter 'exit' to quit");
+
+            Console.WriteLine("Welcome to the quilting pattern app! Type 'triangle' to add a triangle to the pattern. Enter 'exit' to quit");
             string userCommand = Console.ReadLine();
             while (isRunning == true)
             {
@@ -18,6 +21,18 @@ namespace QuiltingPatternApp
                         Console.WriteLine("goodbye!");
                         isRunning = false;
                         break;
+
+                    case "triangle":
+                        Console.WriteLine("What size?");
+                        shapeLength = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("What color?");
+                        shapecolor = Console.ReadLine();
+                        Triangle T = new Triangle(shapeLength, shapecolor);
+                        Console.WriteLine($"you just added a Triangle size {T.Length} and color {T.Color}!");
+                        Console.WriteLine($"would you like to enter another shape to the pattern?");
+                        userCommand = Console.ReadLine();
+                        break;
+
 
                     default:
                         Console.WriteLine("unrecognized command! please try again.");
