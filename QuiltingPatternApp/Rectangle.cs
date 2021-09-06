b09649e3831737a49a550142829db36decd3204b
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-06 15:53:10 -0500

    adds 'rectangle' subclass

---
+++ QuiltingPatternApp/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiltingPatternApp
+{
+    class Rectangle : Shape
+    {
+        public int Width { get; set; }
+        public Rectangle(int lengthInput, int widthInput, string colorInput)
+        {
+            this.Name = "Rectangle";
+            this.Length = lengthInput;
+            this.Width = widthInput;
+            this.Color = colorInput;
+        }
+    }
+}
