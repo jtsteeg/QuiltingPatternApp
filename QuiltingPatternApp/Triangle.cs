369eedfe232def1814b5db1bb54fb9031a5e5a47
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-06 14:37:46 -0500

    Adds Triangle subclass

---
+++ QuiltingPatternApp/Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiltingPatternApp
+{
+    class Triangle : Shape
+    {
+        public Triangle(int lengthInput, string colorInput)
+        {
+            this.numberOfSides = 3;
+            this.Length = lengthInput;
+            this.Color = colorInput;
+        }
+    }
+}
