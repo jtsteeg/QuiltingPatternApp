6aaeaaee3e3b77ea02f441249086ff0166ed1a77
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-09-06 13:24:13 -0500

    Initial Commit

---
+++ QuiltingPatternApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace QuiltingPatternApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            Console.WriteLine("Welcome to the quilting pattern app! Enter 'exit' to quit");
+            string userCommand = Console.ReadLine();
+            while (isRunning == true)
+            {
+                switch (userCommand)
+                {
+                    case "exit":
+                        Console.WriteLine("goodbye!");
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("unrecognized command! please try again.");
+                        userCommand = Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
